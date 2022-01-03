eb3d14c223873f62e61fe5d894390f83da4a2548
ccosneauampis <94106855+ccosneauampis@users.noreply.github.com>
2022-01-03 12:03:20 +0100

    Changed and updated style

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Burger menu > < Navigation Menu
 function menuToggle() {
   var x = document.getElementById("myNavtoggle");
   if (x.className === "navtoggle") {
@@ -7,14 +8,27 @@ function menuToggle() {
   }
 }
 
+//Hiding nav bar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myNavtoggle").style.top = "0";
+  } else {
+    document.getElementById("myNavtoggle").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+//Back-to-top Arrow
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down n px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 625 || document.documentElement.scrollTop > 625) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
